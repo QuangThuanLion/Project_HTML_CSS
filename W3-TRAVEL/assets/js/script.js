4583c18dc6ab9b12776bbc91fe548f28683036f5
Nguyen Khac. Thuan <thuannk@dsoft.com.vn>
2021-07-23 16:20:55 +0700

    Thuan update source code 20212307

--- W3-TRAVEL/assets/js/script.js
+++ W3-TRAVEL/assets/js/script.js
@@ -19,3 +19,22 @@ function checkScroll() {
         })
     }
 }
+
+// MENU MOBILE
+var menu__mobile = document.getElementById("menu__mobile");
+var header__nav__wrapper = document.getElementById("header__nav__wrapper");
+var menu__mobile_icon = menu__mobile.firstElementChild;
+var isClosed = true;
+
+menu__mobile.onclick = (event) => {
+    event.preventDefault();
+    if (isClosed) {
+        header__nav__wrapper.style.display = "block";
+        menu__mobile.style.backgroundColor = "#000";
+        menu__mobile_icon.setAttribute("style", "color: #fff;");
+        isClosed = false;
+    } else {
+        header__nav__wrapper.style.display = null;
+        isClosed = true;
+    }
+}
